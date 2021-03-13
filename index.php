6bdea2168fd834c2ef290d141911bce72967211e
Litvinov Valery <va-litv@yandex.ru>
2021-03-13 12:11:51 +0300

    add posts array with data

--- index.php
+++ index.php
@@ -1,7 +1,45 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = 'Валерий'; // укажите здесь ваше имя
+$user_name = 'Валерий';
+$posts = [
+    [
+        'title' => 'Цитата',
+        'type' => 'post-quote',
+        'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+        'user_name' => 'Лариса',
+        'user_picture' => 'userpic-larisa-small.jpg'
+    ],
+    [
+        'title' => 'Игра престолов',
+        'type' => 'post-text',
+        'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
+        'user_name' => 'Владик',
+        'user_picture' => 'userpic.jpg'
+    ],
+    [
+        'title' => 'Наконец, обработал фотки!',
+        'type' => 'post-photo',
+        'content' => 'rock-medium.jpg',
+        'user_name' => 'Виктор',
+        'user_picture' => 'userpic-mark.jpg'
+    ],
+    [
+        'title' => 'Моя мечта',
+        'type' => 'post-photo',
+        'content' => 'coast-medium.jpg',
+        'user_name' => 'Лариса',
+        'user_picture' => 'userpic-larisa-small.jpg'
+    ],
+    [
+        'title' => 'Лучшие курсы',
+        'type' => 'post-link',
+        'content' => 'www.htmlacademy.ru',
+        'user_name' => 'Владик',
+        'user_picture' => 'userpic.jpg'
+    ]
+]
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
