9f883a20bc7d9c8ad07d0c0d6fe2c9b0012f45e2
Litvinov Valery <va-litv@yandex.ru>
2021-03-13 16:51:15 +0300

    added semicolon and comma to last array elements

--- index.php
+++ index.php
@@ -8,37 +8,37 @@ $posts = [
         'type' => 'post-quote',
         'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
         'user_name' => 'Лариса',
-        'user_picture' => 'userpic-larisa-small.jpg'
+        'user_picture' => 'userpic-larisa-small.jpg',
     ],
     [
         'title' => 'Игра престолов',
         'type' => 'post-text',
         'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
         'user_name' => 'Владик',
-        'user_picture' => 'userpic.jpg'
+        'user_picture' => 'userpic.jpg',
     ],
     [
         'title' => 'Наконец, обработал фотки!',
         'type' => 'post-photo',
         'content' => 'rock-medium.jpg',
         'user_name' => 'Виктор',
-        'user_picture' => 'userpic-mark.jpg'
+        'user_picture' => 'userpic-mark.jpg',
     ],
     [
         'title' => 'Моя мечта',
         'type' => 'post-photo',
         'content' => 'coast-medium.jpg',
         'user_name' => 'Лариса',
-        'user_picture' => 'userpic-larisa-small.jpg'
+        'user_picture' => 'userpic-larisa-small.jpg',
     ],
     [
         'title' => 'Лучшие курсы',
         'type' => 'post-link',
         'content' => 'www.htmlacademy.ru',
         'user_name' => 'Владик',
-        'user_picture' => 'userpic.jpg'
-    ]
-]
+        'user_picture' => 'userpic.jpg',
+    ],
+];
 
 ?>
 <!DOCTYPE html>
