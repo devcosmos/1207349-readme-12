dc11fe658826ccf5ba8c6efc45e00e97af31d356
Litvinov Valery <va-litv@yandex.ru>
2021-03-30 14:29:27 +0300

    add foreign key to table and add index to db

--- schema.sql
+++ schema.sql
@@ -1,6 +1,5 @@
 CREATE DATABASE readme
-  DEFAULT CHARACTER SET utf8
-  DEFAULT COLLATE utf8_general_ci;
+  DEFAULT CHARACTER SET utf8mb4;
 
 USE readme;
 
@@ -8,14 +7,22 @@ CREATE TABLE users (
   id INT AUTO_INCREMENT PRIMARY KEY,
   dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   email VARCHAR(128) NOT NULL UNIQUE,
-  login VARCHAR(128) NOT NULL UNIQUE,
-  password CHAR(64) NOT NULL,
+  username VARCHAR(128) NOT NULL,
+  password VARCHAR(64) NOT NULL,
   avatar_path VARCHAR(255)
+);
+
+CREATE TABLE content_types (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL UNIQUE,
+  icon_class VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE posts (
   id INT AUTO_INCREMENT PRIMARY KEY,
   dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  user_id INT,
+  content_type_id INT,
   title VARCHAR(255),
   content TEXT,
   quote_author VARCHAR(255),
@@ -23,9 +30,8 @@ CREATE TABLE posts (
   video_path VARCHAR(255),
   link_path VARCHAR(255),
   show_count INT,
-  user_id INT,
-  content_type_id INT,
-  hashtag_id INT
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (content_type_id) REFERENCES content_types(id)
 );
 
 CREATE TABLE comments (
@@ -33,19 +39,25 @@ CREATE TABLE comments (
   dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   content TEXT,
   user_id INT,
-  post_id INT
+  post_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (post_id) REFERENCES posts(id)
 );
 
 CREATE TABLE likes (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT,
-  post_id INT
+  post_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (post_id) REFERENCES posts(id)
 );
 
 CREATE TABLE subscribers (
   id INT AUTO_INCREMENT PRIMARY KEY,
   subscriber_id INT,
-  user_id INT
+  user_id INT,
+  FOREIGN KEY (subscriber_id) REFERENCES users(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE messages (
@@ -53,16 +65,23 @@ CREATE TABLE messages (
   dt_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   content TEXT,
   sender_id INT,
-  recipient_id INT
+  recipient_id INT,
+  FOREIGN KEY (sender_id) REFERENCES users(id),
+  FOREIGN KEY (recipient_id) REFERENCES users(id)
 );
 
 CREATE TABLE hashtags (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255)
+  title VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE content_types (
+CREATE TABLE post_tags (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255),
-  icon_class VARCHAR(255)
+  post_id INT,
+  hashtag_id INT,
+  FOREIGN KEY (post_id) REFERENCES posts(id),
+  FOREIGN KEY (hashtag_id) REFERENCES hashtags(id)
 );
+
+CREATE INDEX content_type ON content_types(title);
+CREATE INDEX hashtag ON hashtags(title);
