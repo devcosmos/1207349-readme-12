88eccd064f99718dd90a64b3a1e40f313f9cc6da
Litvinov Valery <va-litv@yandex.ru>
2021-04-15 17:09:42 +0300

    made like count in separate request

--- post.php
+++ post.php
@@ -5,15 +5,19 @@ $post_id = $_GET['post_id'] ?? 0;
 $post_id = intval($post_id);
 
 $sql_select_post_by_id = '
-    SELECT p.id, p.title, p.content, u.username, u.user_picture, ct.type_class, COUNT(l.post_id) AS like_count
+    SELECT p.id, p.title, p.content, u.username, u.user_picture, ct.type_class
       FROM posts AS p 
       JOIN users AS u ON p.user_id = u.id
-      JOIN content_types AS ct ON p.content_type_id = ct.id 
- LEFT JOIN likes AS l ON p.id = l.post_id
-     WHERE p.id = ? 
-     GROUP BY p.id';
+      JOIN content_types AS ct ON p.content_type_id = ct.id
+     WHERE p.id = ?';
+$sql_select_like_count = '
+    SELECT COUNT(post_id) AS count
+      FROM likes
+     WHERE post_id = ? 
+     GROUP BY post_id';
 
 $post = select_query_with_stmt_and_fetch($db, $sql_select_post_by_id, 'i', [$post_id], false);
+$like_count = select_query_with_stmt_and_fetch($db, $sql_select_like_count, 'i', [$post_id], false);
 
 if ($post['id'] === NULL || $post_id === 0) {
     get_error_code(404);
@@ -25,6 +29,7 @@ $post_content = include_template('post/post-' . $post['type_class'] . '.php', [
 $main_content = include_template('post-details.php', [
     'post_content' => $post_content,
     'post' => $post,
+    'like_count' => $like_count['count'],
 ]);
 $layout_content = include_template('layout.php', [
     'content' => $main_content, 
