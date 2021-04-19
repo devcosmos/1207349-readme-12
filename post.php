ba7d10f972bcbc5046fcfae65eb684a08b175caf
Litvinov Valery <va-litv@yandex.ru>
2021-04-19 12:16:15 +0300

    add error template

--- post.php
+++ post.php
@@ -2,40 +2,49 @@
 require 'common.php';
 
 $post_id = $_GET['post_id'] ?? 0;
-$post_id = intval($post_id);
 
-$sql_select_post_by_id = '
-    SELECT p.id, p.title, p.content, u.username, u.user_picture, ct.type_class,
-    (SELECT count(*) FROM likes WHERE post_id = p.id) AS like_count, 
-    (SELECT count(*) FROM comments WHERE post_id = p.id) AS comment_count
-      FROM posts AS p 
-      JOIN users AS u ON p.user_id = u.id
-      JOIN content_types AS ct ON p.content_type_id = ct.id
-     WHERE p.id = ?
-';
-$sql_select_comments_from_post = '
-    SELECT u.username, u.user_picture, c.content, c.dt_add
-      FROM comments AS c
-      JOIN users AS u ON c.user_id = u.id
-     WHERE c.post_id = ?
-';
+if ($post_id === 0) {
+    $main_content = include_template('error.php');
+    $title = 'Страница не найдена';
+} else {
+    $post_id = intval($post_id);
 
-$post = select_query($db, $sql_select_post_by_id, [$post_id], 'i')->fetch_assoc();
-if (empty($post) || $post_id === 0) {
-    get_error_code(404);
+    $sql_select_post_by_id = '
+        SELECT p.id, p.title, p.content, u.username, u.user_picture, ct.type_class,
+        (SELECT count(*) FROM likes WHERE post_id = p.id) AS like_count, 
+        (SELECT count(*) FROM comments WHERE post_id = p.id) AS comment_count
+        FROM posts AS p 
+        JOIN users AS u ON p.user_id = u.id
+        JOIN content_types AS ct ON p.content_type_id = ct.id
+        WHERE p.id = ?
+    ';
+    $sql_select_comments_from_post = '
+        SELECT u.username, u.user_picture, c.content, c.dt_add
+        FROM comments AS c
+        JOIN users AS u ON c.user_id = u.id
+        WHERE c.post_id = ?
+    ';
+
+    $post = select_query($db, $sql_select_post_by_id, [$post_id], 'i')->fetch_assoc();
+
+    if (!$post) {
+        $main_content = include_template('error.php');
+        $title = 'Страница не найдена';
+    } else {
+        $post_comments = select_query($db, $sql_select_comments_from_post, [$post_id], 'i')->fetch_all(MYSQLI_ASSOC);
+
+        $title = $post['title'];
+    
+        $post_content = include_template('post/post-' . $post['type_class'] . '.php', ['post' => $post]);
+        $post_comments_content = include_template('post/post-comments.php', ['post_comments' => $post_comments]);
+        $main_content = include_template('post-details.php', [
+            'post_content' => $post_content,
+            'post_comments_content' => $post_comments_content,
+            'post' => $post,
+        ]);
+    }
 }
 
-$post_comments = select_query($db, $sql_select_comments_from_post, [$post_id], 'i')->fetch_all(MYSQLI_ASSOC);
-
-$title = $post['title'];
-
-$post_content = include_template('post/post-' . $post['type_class'] . '.php', ['post' => $post]);
-$post_comments_content = include_template('post/post-comments.php', ['post_comments' => $post_comments]);
-$main_content = include_template('post-details.php', [
-    'post_content' => $post_content,
-    'post_comments_content' => $post_comments_content,
-    'post' => $post,
-]);
 $layout_content = include_template('layout.php', [
     'content' => $main_content, 
     'is_auth' => $is_auth, 
---
+++ templates/error.php
@@ -0,0 +1,17 @@
+<main class="page__main page__main--search-results">
+  <h1 class="visually-hidden"><?= $error; ?></h1>
+  <section class="search">
+    <h2 class="visually-hidden">Результаты поиска</h2>
+    <div class="search__results-wrapper">
+      <div class="search__no-results container">
+        <p class="search__no-results-info">К сожалению, страница не найдена.</p>
+        <p class="search__no-results-desc">
+          Возможно в адресе допущена ошибка или нужная вам страница была удалена.
+        </p>
+        <div class="search__links">
+          <a class="search__popular-link button button--main" href="/">Вернуться на главную</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
