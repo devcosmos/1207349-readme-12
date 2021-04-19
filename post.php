6525b071e9cd40cefa0cd33376cfef96226b8ab8
Litvinov Valery <va-litv@yandex.ru>
2021-04-19 13:27:04 +0300

    refactored post.php

--- post.php
+++ post.php
@@ -1,48 +1,45 @@
 <?php
 require 'common.php';
 
-$error_404 = false;
 $post_id = $_GET['post_id'] ?? 0;
+$post_id = intval($post_id);
 
 if ($post_id === 0) {
     get_error_code(404);
-} else {
-    $post_id = intval($post_id);
+} 
 
-    $sql_select_post_by_id = '
-        SELECT p.id, p.title, p.content, u.username, u.user_picture, ct.type_class,
-        (SELECT count(*) FROM likes WHERE post_id = p.id) AS like_count, 
-        (SELECT count(*) FROM comments WHERE post_id = p.id) AS comment_count
-        FROM posts AS p 
-        JOIN users AS u ON p.user_id = u.id
-        JOIN content_types AS ct ON p.content_type_id = ct.id
-        WHERE p.id = ?
-    ';
-    $sql_select_comments_from_post = '
-        SELECT u.username, u.user_picture, c.content, c.dt_add
-        FROM comments AS c
-        JOIN users AS u ON c.user_id = u.id
-        WHERE c.post_id = ?
-    ';
+$sql_select_post_by_id = '
+    SELECT p.id, p.title, p.content, u.username, u.user_picture, ct.type_class,
+    (SELECT count(*) FROM likes WHERE post_id = p.id) AS like_count, 
+    (SELECT count(*) FROM comments WHERE post_id = p.id) AS comment_count
+    FROM posts AS p 
+    JOIN users AS u ON p.user_id = u.id
+    JOIN content_types AS ct ON p.content_type_id = ct.id
+    WHERE p.id = ?
+';
+$sql_select_comments_from_post = '
+    SELECT u.username, u.user_picture, c.content, c.dt_add
+    FROM comments AS c
+    JOIN users AS u ON c.user_id = u.id
+    WHERE c.post_id = ?
+';
 
-    $post = select_query($db, $sql_select_post_by_id, [$post_id], 'i')->fetch_assoc();
+$post = select_query($db, $sql_select_post_by_id, [$post_id], 'i')->fetch_assoc();
 
-    if ($post) {
-        $post_comments = select_query($db, $sql_select_comments_from_post, [$post_id], 'i')->fetch_all(MYSQLI_ASSOC);
-
-        $title = $post['title'];
-    
-        $post_content = include_template('post/post-' . $post['type_class'] . '.php', ['post' => $post]);
-        $post_comments_content = include_template('post/post-comments.php', ['post_comments' => $post_comments]);
-        $main_content = include_template('post-details.php', [
-            'post_content' => $post_content,
-            'post_comments_content' => $post_comments_content,
-            'post' => $post,
-        ]);
-    } else {
-        get_error_code(404);
-    }
+if (!$post) {
+    get_error_code(404);
 }
+$post_comments = select_query($db, $sql_select_comments_from_post, [$post_id], 'i')->fetch_all(MYSQLI_ASSOC);
+
+$title = $post['title'];
+
+$post_content = include_template('post/post-' . $post['type_class'] . '.php', ['post' => $post]);
+$post_comments_content = include_template('post/post-comments.php', ['post_comments' => $post_comments]);
+$main_content = include_template('post-details.php', [
+    'post_content' => $post_content,
+    'post_comments_content' => $post_comments_content,
+    'post' => $post,
+]);
 
 $layout_content = include_template('layout.php', [
     'content' => $main_content, 
